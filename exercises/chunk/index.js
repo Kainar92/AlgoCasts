e205c50818930ec40ac69bd5fece54c2a33e55f8
Kainar Ilyassov <k.ilyassov@infin-it.kz>
2020-05-12 13:01:10 +0600

    added chunk second method of soling

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -10,20 +10,34 @@
 
 function chunk(array, size) {
     let chunked = [];
+    let index = 0;
 
-    for(let el of array) {
-        const last = chunked[chunked.length - 1];
-        
-        if(!last || last.length === size) {
-            chunked.push([el])
-        } else {
-            last.push(el);
-        }
+    while(index < array.length) {
+       let subArray = array.slice(index, index + size)
+       chunked.push(subArray);
+       index += size;
     }
 
-    return chunked
+    return chunked;
 }
 
 console.log(chunk([1,2,3,4,5,6,7,8,9], 2))
 
 module.exports = chunk;
+
+// #1
+// function chunk(array, size) {
+//     let chunked = [];
+
+//     for(let el of array) {
+//         const last = chunked[chunked.length - 1];
+        
+//         if(!last || last.length === size) {
+//             chunked.push([el])
+//         } else {
+//             last.push(el);
+//         }
+//     }
+
+//     return chunked
+// }
